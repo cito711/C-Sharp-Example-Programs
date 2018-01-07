969fa6a475f62c86d67abc38fba73b32d589c4e8
cito711 <garciape711github@hotmail.com>
2018-01-07 16:34:31 -0600

    Simple Program taking user input to display greeting

---
+++ Ask Name.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.Write("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name + " it is nice to meet you");
+
+        }
+    }
+}
